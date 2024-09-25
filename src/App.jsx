21601ec4a1c10e4823aaf2ade275be48fc9a3796
Kerem Tazedal <kerem.tazedal.89@gmail.com>
2024-09-25 12:27:58 +0200

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import React from 'react';
 import Header from './header'; // Adjust the path according to your file structure
+import List from './list'; // Adjust the path according to your file structure
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* Other components */}
+      <List />
     </div>
   );
 }
