e7d016ea88edefb7c4ed34780e443eadd906b091
Kerem Tazedal <kerem.tazedal.89@gmail.com>
2024-09-25 11:46:16 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import React from 'react';
+import Header from './header'; // Adjust the path according to your file structure
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+export default function SimpleHeader() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-blue-100">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt="Company Logo"
+              src="src\assets\logo.webp" 
+              className="h-10 w-auto"
+            />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            {mobileMenuOpen ? (
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            ) : (
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            )}
+          </button>
+        </div>
+        <div className={`lg:flex lg:gap-x-12 ${mobileMenuOpen ? 'block' : 'hidden'} lg:block`}>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Recept
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Mat
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Kontakt
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
