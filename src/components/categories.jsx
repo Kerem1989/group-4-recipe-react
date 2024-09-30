a3b51354048efb61465e1497c3c5c21d7bb36020
HomanAgh <145568266+HomanAgh@users.noreply.github.com>
2024-09-30 22:43:54 +0200

    fler recept till json-filen och lekt lite med categories

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 
 function Categories() {
   return (
@@ -10,6 +10,25 @@ function Categories() {
         <li>Huvudrätter</li>
         <li>Desserter</li>
         <li>Snacks</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories; */
+
+import React from "react";
+
+function Categories() {
+  return (
+    <div className="categories p-4">
+      <h1 className="text-4xl font-bold text-center">Kategorier</h1>
+      <p className="text-center mt-4">Utforska våra olika kategorier av recept!</p>
+      <ul className="list-disc list-inside mt-4">
+        {/* <li>Förrätter</li>
+        <li>Huvudrätter</li>
+        <li>Desserter</li>
+        <li>Snacks</li> */}
       </ul>
     </div>
   );
