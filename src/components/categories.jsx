69968c75d8cf1d6e5cd484acec779fe754bac0c3
HomanAgh <145568266+HomanAgh@users.noreply.github.com>
2024-10-02 15:55:19 +0200

    nya categories och ändrat i categories.jsx

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -25,10 +25,35 @@ function Categories() {
       <h1 className="text-4xl font-bold text-center">Kategorier</h1>
       <p className="text-center mt-4">Utforska våra olika kategorier av recept!</p>
       <ul className="list-disc list-inside mt-4">
-        {/* <li>Förrätter</li>
+        <li>Förrätter</li>
         <li>Huvudrätter</li>
-        <li>Desserter</li>
-        <li>Snacks</li> */}
+        <li>..</li>
+        <li>..</li>
+      </ul>
+      <ul className="list-disc list-inside mt-4">
+        <li>Kött</li>
+        <li>Fisk och Skaldjur</li>
+        <li>Vegetariskt</li>
+        <li>Veganskt</li>
+        <li>Glutenfritt</li>
+      </ul>
+      <ul className="list-disc list-inside mt-4">
+        <li>Husmanskost</li>
+        <li>Internationellt</li>
+      </ul>
+      <ul className="list-disc list-inside mt-4">
+        <li>15 min</li>
+        <li>30 min</li>
+        <li>45 min</li>
+        <li>60 min</li>
+        <li>90 min</li>
+        <li>120 min</li>
+      </ul>
+      <ul className="list-disc list-inside mt-4">
+        <li>50 SEK</li>
+        <li>100 SEK</li>
+        <li>150 SEK</li>
+        <li>200 SEK</li>
       </ul>
     </div>
   );
